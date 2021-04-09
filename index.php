fd587423e60cc1884e936a110b174816f90c3030
apgit33 <adrienpaturot@yahoo.fr>
2021-04-09 10:31:49 +0200

    Update test index

--- index.php
+++ index.php
@@ -3,19 +3,15 @@ require dirname(__FILE__) . DIRECTORY_SEPARATOR . "vendor/autoload.php";
 
 use App\Mail;
 
+$mail = new Mail();
 
-    $mail = new Mail();
+$mail->setSender("admin@admid.fr");
+$mail->add('recipients', ['test@test.fr','admin@test.fr','test2@test.fr']);
+$mail->add('cc', 'cc@cc.fr');
+$mail->add('bcc', 'bcc@bcc.fr');
+$mail->setSubject('test_mail');
+$mail->setBody('<h1>Title</h1>');
+$mail->setType('html');
+$mail->addAttachment(['upload/html.txt','upload/html_copy.txt']);
 
-    $mail->setSender("admin@admid.fr");
-    // $mail->addRecipient(['test@test.fr','admi@test.fr','tddest@test.fr']);
-    // $mail->deleteRecipient('admi@test.fr');
-
-    // $mail->addCc(['aa@test.fr','aaa@test.fr']);
-    // $mail->addBcc(['bb@test.fr','bbb@testsqsq.fr']);
-    $mail->add('recipients', 'test@test.fr');
-    $mail->setSubject('test_mail');
-    $mail->setBody('<h1>Title</h1>');
-    $mail->setType('html');
-    // $mail->addAttachment(['html.txt','html_copy.txt']);
-
-    print_r($mail->send());
+print_r($mail->send());
